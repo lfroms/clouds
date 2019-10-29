65b632e0a9a418edc4abc6e5b4ece09188506c28
Lukas Romsicki <lukas@romsicki.com>
2019-10-28 21:58:04 -0400

    Reduce fragility of TabControl

--- Forecast/Views/Navigation Bar/Tab Control/TabControl.swift
+++ Forecast/Views/Navigation Bar/Tab Control/TabControl.swift
@@ -10,58 +10,92 @@ import SwiftUI
 
 struct TabControl: View {
     @Binding var activeTab: Int
+
     let tabs: [String]
 
-    @State private var tabWidths: [CGFloat] = [0, 0, 0]
-    @State private var tabHeights: [CGFloat] = [0, 0, 0]
+    @State private var tabWidths: DimensionDictionary = [:]
+    @State private var tabHeights: DimensionDictionary = [:]
+    @State private var tabOffsets: DimensionDictionary = [:]
 
-    @State private var tabOffsets: [CGFloat] = [0, 0, 0]
+    private typealias DimensionDictionary = [Int: CGFloat]
 
     var body: some View {
         ZStack(alignment: Alignment.leading) {
             RoundedRectangle(cornerRadius: .infinity)
-                .frame(width: tabWidths[activeTab], height: tabHeights[activeTab])
-                .offset(x: tabOffsets[activeTab])
-                .foregroundColor(Color.primary.opacity(0.15))
-                .animation(.spring(response: 0.3, dampingFraction: 0.7, blendDuration: 0))
+                .frame(width: activeTabSize.width, height: activeTabSize.height)
+                .offset(x: activeTabOffset)
+                .foregroundColor(Self.indicatorColor)
+                .animation(Self.tabHighlightAnimation)
 
             HStack(alignment: .firstTextBaseline, spacing: 0) {
                 ForEach(tabs, id: \.self) { tab in
                     TabView(
                         text: tab,
-                        action: self.didPressTab(named: tab),
-                        isActive: self.isActive(tab: tab)
+                        action: self.didPressTab(tab),
+                        isActive: self.isActive(tab)
                     )
-                        .padding(.vertical, 6)
+                    .padding(.vertical, 6)
                     .padding(.horizontal, 16)
                     .fixedSize()
                     .background(TabGeometry(xOffset: 20))
                     .onPreferenceChange(TabWidthPreferenceKey.self, perform: {
-                        self.tabWidths[self.indexOfTab(named: tab)] = $0
+                        self.tabWidths[self.indexOfTab(tab)] = $0
                     })
                     .onPreferenceChange(TabHeightPreferenceKey.self, perform: {
-                        self.tabHeights[self.indexOfTab(named: tab)] = $0
+                        self.tabHeights[self.indexOfTab(tab)] = $0
                     })
                     .onPreferenceChange(TabOffsetPreferenceKey.self, perform: {
-                        self.tabOffsets[self.indexOfTab(named: tab)] = $0
+                        self.tabOffsets[self.indexOfTab(tab)] = $0
                     })
                 }
             }
         }
     }
 
-    private func didPressTab(named tab: String) -> () -> Void {
+    private var activeTabSize: CGSize {
+        .init(
+            width: getWithDefaultValue(from: tabWidths, key: activeTab),
+            height: getWithDefaultValue(from: tabHeights, key: activeTab)
+        )
+    }
+
+    private var activeTabOffset: CGFloat {
+        getWithDefaultValue(from: tabOffsets, key: activeTab)
+    }
+
+    private func getWithDefaultValue(from dictionary: DimensionDictionary, key: Int) -> DimensionDictionary.Value {
+        dictionary[key, default: 0]
+    }
+
+    private static var tabHighlightAnimation: Animation {
+        .spring(
+            response: 0.3,
+            dampingFraction: 0.7,
+            blendDuration: 0
+        )
+    }
+
+    private static var indicatorColor: Color {
+        Color.primary.opacity(0.15)
+    }
+
+    private func didPressTab(_ tab: String) -> () -> Void {
         return {
-            self.activeTab = self.indexOfTab(named: tab)
+            if !self.isActive(tab) {
+                let generator = UISelectionFeedbackGenerator()
+                generator.selectionChanged()
+            }
+
+            self.activeTab = self.indexOfTab(tab)
         }
     }
 
-    private func indexOfTab(named tab: String) -> Int {
+    private func indexOfTab(_ tab: String) -> Int {
         return tabs.firstIndex(of: tab) ?? 0
     }
 
-    private func isActive(tab: String) -> Bool {
-        return indexOfTab(named: tab) == activeTab
+    private func isActive(_ tab: String) -> Bool {
+        return indexOfTab(tab) == activeTab
     }
 }
 
