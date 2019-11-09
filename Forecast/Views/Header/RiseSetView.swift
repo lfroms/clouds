8b03682fe56ceb7fb7ccb5c23ecf7ed0f773325f
Lukas Romsicki <lukas@romsicki.com>
2019-11-09 17:09:47 -0500

    Show sunrise and sunset

--- Forecast/Views/Header/RiseSetView.swift
+++ Forecast/Views/Header/RiseSetView.swift
@@ -6,17 +6,56 @@
 //  Copyright © 2019 Lukas Romsicki. All rights reserved.
 //
 
+import SwiftDate
 import SwiftUI
 
 struct RiseSetView: View {
+    @EnvironmentObject private var provider: WeatherProvider
+
     var body: some View {
         HStack(alignment: .lastTextBaseline, spacing: 5) {
-            Image(systemName: "sunset.fill").padding(.bottom, 3)
+            Image(systemName: symbolName).padding(.bottom, 3)
 
-            Text("18:20 EST")
+            Text(text)
         }
         .font(Font.subheadline.weight(.semibold))
         .foregroundColor(.secondary)
+    }
+
+    private var symbolName: String {
+        guard let sun = provider.weather?.sun else {
+            return ""
+        }
+
+        let riseTime = inUTCTime(time: sun.riseTime)
+        let setTime = inUTCTime(time: sun.setTime)
+        let currentTime = Date()
+
+        if currentTime.isInRange(date: riseTime.date, and: setTime.date) {
+            return "sunset.fill"
+        }
+
+        return "sunrise.fill"
+    }
+
+    private func inUTCTime(time: Int) -> DateInRegion {
+        DateInRegion(seconds: TimeInterval(time), region: .UTC)
+    }
+
+    private var text: String {
+        guard let sun = provider.weather?.sun else {
+            return ""
+        }
+
+        let riseTime = inUTCTime(time: sun.riseTime).convertTo(region: .current)
+        let setTime = inUTCTime(time: sun.setTime).convertTo(region: .current)
+        let currentTime = Date().convertTo(region: .current)
+
+        if currentTime.isInRange(date: riseTime, and: setTime) {
+            return setTime.toFormat("h:mm a z")
+        }
+
+        return riseTime.toFormat("h:mm a z")
     }
 }
 
