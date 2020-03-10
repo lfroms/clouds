89b72526fbf7eb38719c558ed338ed04813e722f
Lukas Romsicki <lukas@romsicki.com>
2020-03-10 11:14:56 -0400

    Quick refactor of location picker

--- Forecast/Sections/Location/LocationPicker/LocationPicker.swift
+++ Forecast/Sections/Location/LocationPicker/LocationPicker.swift
@@ -8,49 +8,13 @@
 
 import SwiftUI
 
-struct LocationPicker {
-    @Binding private(set) var appState: AppState
-    @Binding private(set) var weatherService: WeatherService
-    @Binding private(set) var locationService: LocationService
-    @Binding private(set) var locationSearchService: LocationSearchService
-    @Binding private(set) var locationFavoritesService: LocationFavoritesService
-
-    let didPerformDismiss: (() -> Void)?
-
-    private var currentLocationName: String? {
-        guard let placemark = locationService.lastPlacemark else {
-            return nil
-        }
-
-        return placemark.locality
+struct LocationPicker: Equatable {
+    static func == (lhs: LocationPicker, rhs: LocationPicker) -> Bool {
+        lhs.data == rhs.data
     }
-
-    private var currentRegionName: String? {
-        guard let placemark = locationService.lastPlacemark else {
-            return nil
-        }
-
-        return LocationNameHelper.shared.createRegionNameFrom(placemark: placemark)
-    }
-
-    private var locationPickerData: LocationPickerData {
-        var currentLocation: Location?
-
-        if let name = currentLocationName, let regionName = currentRegionName, let location = locationService.lastLocation {
-            currentLocation = Location(name: name, regionName: regionName, coordinate: location.coordinate)
-        }
-
-        return LocationPickerData(
-            currentLocation: currentLocation,
-            favoriteLocations: locationFavoritesService.favoriteLocations,
-            searchQuery: locationSearchService.searchQuery,
-            state: locationSearchService.searchQuery.isEmpty ? .normal : .searching,
-            searchResults: locationSearchService.results,
-            loadingCurrentLocation: weatherService.loading,
-            loadingFavorites: weatherService.loading,
-            loadingSearch: locationSearchService.loading
-        )
-    }
+    
+    var data: LocationPickerData
+    var didPerformDismiss: () -> Void
 }
 
 extension LocationPicker: UIViewControllerRepresentable {
@@ -58,12 +22,12 @@ extension LocationPicker: UIViewControllerRepresentable {
 
     internal func makeUIViewController(context: Context) -> UIViewControllerType {
         let viewController = UIViewControllerType()
-        viewController.data = locationPickerData
-        viewController.didPerformDismiss = didPerformDismiss
+        viewController.data = data
+        viewController.didPerformDismiss = self.didPerformDismiss
         return viewController
     }
 
     internal func updateUIViewController(_ viewController: UIViewControllerType, context: Context) {
-        viewController.data = locationPickerData
+        viewController.data = data
     }
 }
--- Forecast/Sections/Location/LocationPickerSection.swift
+++ Forecast/Sections/Location/LocationPickerSection.swift
@@ -9,12 +9,47 @@
 import SwiftUI
 
 struct LocationPickerSection: View {
-    @EnvironmentObject private var appState: AppState
     @EnvironmentObject private var locationPickerState: LocationPickerState
+
     @EnvironmentObject private var weatherService: WeatherService
     @EnvironmentObject private var locationService: LocationService
     @EnvironmentObject private var locationSearchService: LocationSearchService
     @EnvironmentObject private var locationFavoritesService: LocationFavoritesService
+
+    private var currentLocationName: String? {
+        guard let placemark = locationService.lastPlacemark else {
+            return nil
+        }
+
+        return placemark.locality
+    }
+
+    private var currentRegionName: String? {
+        guard let placemark = locationService.lastPlacemark else {
+            return nil
+        }
+
+        return LocationNameHelper.shared.createRegionNameFrom(placemark: placemark)
+    }
+
+    private var locationPickerData: LocationPickerData {
+        var currentLocation: Location?
+
+        if let name = currentLocationName, let regionName = currentRegionName, let location = locationService.lastLocation {
+            currentLocation = Location(name: name, regionName: regionName, coordinate: location.coordinate)
+        }
+
+        return LocationPickerData(
+            currentLocation: currentLocation,
+            favoriteLocations: locationFavoritesService.favoriteLocations,
+            searchQuery: locationSearchService.searchQuery,
+            state: locationSearchService.searchQuery.isEmpty ? .normal : .searching,
+            searchResults: locationSearchService.results,
+            loadingCurrentLocation: weatherService.loading,
+            loadingFavorites: weatherService.loading,
+            loadingSearch: locationSearchService.loading
+        )
+    }
 
     var body: some View {
         ZStack(alignment: .topLeading) {
@@ -22,24 +57,18 @@ struct LocationPickerSection: View {
                 .clipShape(RoundedCornerShape(cornerRadius: 22, style: .continuous, corners: [.topLeft, .topRight]))
                 .edgesIgnoringSafeArea(.all)
 
-            LocationPicker(
-                appState: .constant(appState),
-                weatherService: .constant(weatherService),
-                locationService: .constant(locationService),
-                locationSearchService: .constant(locationSearchService),
-                locationFavoritesService: .constant(locationFavoritesService),
-                didPerformDismiss: handlePickerDismiss)
+            LocationPicker(data: locationPickerData, didPerformDismiss: handlePickerDismiss)
+                .equatable()
         }
         .offset(searchOffset)
     }
 
     private var searchOffset: CGSize {
-        return CGSize(width: 0, height: self.locationPickerState.presented ? 0 : Dimension.System.screenHeight)
+        return CGSize(width: 0, height: locationPickerState.presented ? 0 : Dimension.System.screenHeight)
     }
 
     private func handlePickerDismiss() {
-        self.locationPickerState.toggleLocationPicker(animated: true)
-        self.locationSearchService.searchQuery.clear()
+        locationPickerState.toggleLocationPicker(animated: true)
     }
 }
 
