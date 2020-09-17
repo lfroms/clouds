f845ed3f097552c45c9c2de6c227d4ff7db67025
Lukas Romsicki <lukas@romsicki.com>
2020-09-16 21:13:12 -0400

    Fix settings sheet crash

--- Clouds/Views/Master/MasterViewContainer.swift
+++ Clouds/Views/Master/MasterViewContainer.swift
@@ -19,6 +19,7 @@ struct MasterViewContainer: Container {
         .equatable()
         .sheet(isPresented: $settingsSheetState.presented) {
             SettingsSection()
+                .environmentObject(self.settingsSheetState)
                 .preferredColorScheme(.dark)
         }
     }
